04ed8a7f29367dbb40a56a9ba4d9ccbaa98456cd
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-26 00:11:58 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,16 +22,6 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
   
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
-
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
-  
   def data
     product = Product.find(params[:id])
     render json: ProductSerializer.serialize(product)
