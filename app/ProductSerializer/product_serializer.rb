c68be5e296688cefabe2e009883a830f0180b139
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-26 00:13:11 +0000

    Automatically backed up by Learn

--- app/ProductSerializer/product_serializer.rb
+++ app/ProductSerializer/product_serializer.rb
@@ -14,3 +14,24 @@
 #     serialized_post += '}'
 #   end
 # end
+
+
+class ProductSerializer
+  def self.serialize(product)
+      #open/begin json object
+      serialized_product = '{'
+      #product attributes
+      serialized_product += '"id": ' + product.id.to_s + ','
+      serialized_product += '"name": "' + product.name + '",'
+      serialized_product += '"price": ' + product.price.to_s + ','
+    if product.inventory
+      serialized_product += '"inventory": ' + product.inventory.to_s + ','
+    end
+    
+    if product.description
+      serialized_product += '"description": "' + product.description + '"'
+    end
+      #close json object
+      serialized_product += '}'
+  end
+end
